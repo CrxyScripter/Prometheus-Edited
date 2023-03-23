c9d20b57814afeda8816dd46e8cedaf40d5823b8
Crxy <125184554+CrxyScripter@users.noreply.github.com>
2023-03-24 07:23:29 +0800

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -1,12 +1,12 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- test.lua
--- This script contains the Code for the Prometheus CLI
-
--- Configure package.path for requiring Prometheus
+local memoryleaker = [[
+--SCRIPT
+]] 
 local function script_path()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	return str:match("(.*[/%\\])") or "";
 end
 package.path = script_path() .. "?.lua;" .. package.path;
-require("src.cli");
+local op = require("src.cli");
+op:option({
+  script = memoryleaker
+}) 
