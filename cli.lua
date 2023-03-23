68fdd008fb569662b42127ec2348790a995d8c32
Crxy <125184554+CrxyScripter@users.noreply.github.com>
2023-03-24 07:38:29 +0800

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -1,12 +1,15 @@
-local memoryleaker = [[
+local ScriptToObfuscate = [[
 --SCRIPT
 ]] 
+
 local function script_path()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	return str:match("(.*[/%\\])") or "";
 end
+
 package.path = script_path() .. "?.lua;" .. package.path;
-local op = require("src.cli");
-op:option({
-  script = memoryleaker
+
+local Obfuscator = require("src.cli");
+Obfuscator({
+  Script = ScriptToObfuscate
 }) 
