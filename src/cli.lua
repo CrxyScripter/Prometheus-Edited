18b12d6c2f75f999d841f720dcec875f1e868498
Crxy <125184554+CrxyScripter@users.noreply.github.com>
2023-03-24 07:33:10 +0800

    Update cli.lua

--- src/cli.lua
+++ src/cli.lua
@@ -1,15 +1,10 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- cli.lua
--- This script contains the Code for the Prometheus CLI
-
--- Configure package.path for requiring Prometheus
 local function script_path()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	return str:match("(.*[/%\\])")
 end
 package.path = script_path() .. "?.lua;" .. package.path;
----@diagnostic disable-next-line: different-requires
+
+return function(Option) 
 local Prometheus = require("prometheus");
 Prometheus.Logger.logLevel = Prometheus.Logger.LogLevel.Info;
 
@@ -20,25 +15,16 @@ string.split = function(str, sep)
     return fields
 end
 
-local function lines_from(file)
-    if not file_exists(file) then return {} end
-    local lines = {}
-    for line in io.lines(file) do
-      lines[#lines + 1] = line
-    end
-    return lines
-  end
-
--- CLI
 local config;
 local sourceFile = Option.Script
 Prometheus.colors.enabled = false;
 
-local preset = Prometheus.Presets[arg[i]];
+local preset = Prometheus.Presets["Strong"];
 config = preset
 
-local source = table.concat(lines_from(sourceFile), "\n");
+local source = sourceFile
 local pipeline = Prometheus.Pipeline:fromConfig(config);
 local out = pipeline:apply(source, sourceFile);
 
 print(out) 
+end
